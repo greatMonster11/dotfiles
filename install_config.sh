fb5d3ff82fb5a8d519f393625a3cf2664a379273
greatMonster11 <nguyenphuocthanhtn2015@gmail.com>
2021-03-21 13:47:26 +0700

    feat(bugs): update copy command for correct setup

--- install_config.sh
+++ install_config.sh
@@ -4,10 +4,15 @@ set -x
 set -e
 
 mkdir -p ~/config/nvim
+mkdir -p ~/config/nvim/plugin
 
 for f in `find . -regex ".*\.vim$\|.*\.lua$"`; do
     rm -rf ~/.config/nvim/$f
-    cp ./init.vim ~/.config/nvim/$f
+    if [[ $f = "./init.vim" ]]; then
+        cp ./init.vim ~/.config/nvim/$f
+    else  
+        cp $f ~/.config/nvim/plugin/
+    fi
 done
 
 nvim --headless +PlugInstall +qa
